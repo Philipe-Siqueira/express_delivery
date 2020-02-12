e0a0a037a041947c17c3088d9d7fe2716a7aeaf9
Philipe Siqueira (WD5179) <philipe.siqueira@engie.com>
2020-02-12 04:21:57 -0300

    fix host

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app';
 
 const appName = process.env.NODE_APP_NAME || 'Unicad - Delivery Express';
 const PORT = process.env.NODE_APP_PORT || process.env.PORT;
-const HOST = process.env.NODE_APP_HOST || '0.0.0.0';
+const HOST = process.env.NODE_APP_HOST || process.env.HOST;
 const URL = process.env.NODE_APP_URL || 'http://localhost';
 app.listen(PORT, HOST, () => {
   console.log(`Serviço: ${appName} ativo no endereço: ${URL}:${PORT}`);
